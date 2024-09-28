18a7968439bf852109a9ac29c9aac7b24e9f11e6
Mohanmanuhs <mohanmanuhs9@gmail.com>
2024-09-28 19:53:21 +0530

    add Items,profile

--- app/src/main/java/com/example/bnbproject/screens/Page5Screen.kt
+++ app/src/main/java/com/example/bnbproject/screens/Page5Screen.kt
@@ -1,15 +1,106 @@
 package com.example.bnbproject.screens
 
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.bnbproject.ui.components.UserItemCard
+import com.example.bnbproject.viewmodel.ProfileViewModel
+import com.google.firebase.Firebase
+import com.google.firebase.auth.auth
 
+
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Page5Screen(modifier: Modifier = Modifier) {
-    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center){
-        Text(text = "Page5Screen")
+fun Page5Screen(modifier: Modifier = Modifier,profileViewModel: ProfileViewModel = viewModel()) {
+    val firebaseUser by profileViewModel.users.collectAsState()
+    val itemsList by profileViewModel.itemsList.collectAsState()
+
+
+    Firebase.auth.currentUser?.let { profileViewModel.fetchUser(it.uid) }
+    Firebase.auth.currentUser?.let { profileViewModel.fetchUserItems(it.uid) }
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary,
+                ),
+                title = {
+                    Text("Profile")
+                }
+            )
+        },
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxSize()
+                .padding(10.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Spacer(modifier = Modifier.height(20.dp))
+            Card(
+                modifier = Modifier.fillMaxWidth()
+            ) {
+
+                Row(modifier = Modifier.padding(8.dp)) {
+                    Text(text = "Name : ${firebaseUser.name}", fontSize = 16.sp,
+                        fontWeight = FontWeight.Bold
+                    )
+                }
+
+                Row(
+                    modifier = Modifier.padding(8.dp)
+                ) {
+                    Text(text = "Email : ${firebaseUser.email}", fontSize = 16.sp,fontWeight = FontWeight.Bold)
+                }
+            }
+
+            Spacer(modifier = Modifier.height(20.dp))
+
+            Text(
+                modifier = Modifier.fillMaxWidth(),
+                text = "Yours Items :- ",
+                fontSize = 20.sp,
+                textAlign = TextAlign.Center,
+                fontWeight = FontWeight.Bold
+            )
+
+            LazyColumn(
+                modifier = modifier.fillMaxWidth(.95f),
+                horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.spacedBy(5.dp)
+            ) {
+                items(itemsList) {
+                    UserItemCard(it){
+
+                    }
+                }
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/example/bnbproject/ui/components/UserItemCard.kt
@@ -0,0 +1,89 @@
+package com.example.bnbproject.ui.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import coil.compose.rememberAsyncImagePainter
+import com.example.bnbproject.R
+import com.example.bnbproject.model.UserItemModel
+
+@Composable
+fun UserItemCard(userItem: UserItemModel, onClick: () -> Unit) {
+    Card(
+        modifier = Modifier
+            .padding(5.dp)
+            .fillMaxWidth()
+            .clickable { onClick() },
+        shape = RoundedCornerShape(8.dp), elevation = CardDefaults.elevatedCardElevation(defaultElevation = 3.dp)
+    ) {
+        Row(
+            modifier = Modifier.padding(16.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            // Image section
+            Image(
+                painter = if (userItem.img == "") painterResource(id = R.drawable.ic_launcher_background) else rememberAsyncImagePainter(
+                    model = userItem.img
+                ),
+                contentDescription = "dp",
+                modifier = Modifier
+                    .padding(5.dp)
+                    .size(100.dp)
+                    .clip(RectangleShape)
+                    .clickable {
+
+                    },
+                contentScale = ContentScale.Crop
+            )
+            // Details section
+            Spacer(modifier = Modifier.width(16.dp))
+            Column(
+                verticalArrangement = Arrangement.SpaceBetween,
+                modifier = Modifier.weight(1f)
+            ) {
+                Text(
+                    text = userItem.type,
+                    style = MaterialTheme.typography.bodyLarge.copy(fontWeight = FontWeight.Bold),
+                    color = Color.Black
+                )
+                Text(
+                    text = userItem.brand,
+                    style = MaterialTheme.typography.bodyMedium,
+                    color = Color.Gray
+                )
+                Text(
+                    text = userItem.shopFrom,
+                    style = MaterialTheme.typography.bodyMedium,
+                    color = Color.Gray
+                )
+                Text(
+                    text = "$${userItem.price}",
+                    style = MaterialTheme.typography.bodyMedium.copy(fontWeight = FontWeight.Bold),
+                    color = MaterialTheme.colorScheme.primary
+                )
+            }
+        }
+    }
+}
